226bc9a06197ead9361b7219d839e8973e102b62
Tyler Verlinden <tyler.verlinden@go.mnstate.edu>
2019-12-04 10:50:08 -0600

    Add __str__ and test the method

--- Person.py
+++ Person.py
@@ -1,2 +1,5 @@
 class Person:
     lastIdUsed = 100
+
+    def __str__(self):
+        return "{} {} {}".format(self._fName, self._lName, self._id)
--- testPerson.py
+++ testPerson.py
@@ -2,3 +2,7 @@ __author__ = "Tyler Verlinden"
 """
 Description: Program to test the Person class
 """
+
+from Person import Person
+
+print(p1)
